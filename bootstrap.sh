ee7c13a7cb347020cb42f2b988194fc012c26dae
Kun Han <hankun.osu@gmail.com>
2019-12-05 17:16:43 -0800

    remove files

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,9 +40,9 @@ sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ./zsh/gentoo_minimal.zsh-theme ~/.oh-my-zsh/themes/
 cp ./zsh/.zshrc ~/
-source ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+source ~/.zshrc
 
 
 # link to vscode
--- osx.sh
+++ osx.sh
@@ -775,7 +775,7 @@ defaults write com.apple.TextEdit "TabWidth" '4'
 
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
     "Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Messages" \
-    "Opera" "Safari" "SystemUIServer" "iCal"; do
+     "Safari" "SystemUIServer" "iCal"; do
     killall "${app}" > /dev/null 2>&1
 done
 echo "Done. Note that some of these changes require a logout/restart of your OS to take effect.  At a minimum, be sure to restart your Terminal."
